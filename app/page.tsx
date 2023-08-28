150a247405f20889d8154a0f21f1037635dfa004
Vanshaj Kataria <90422718+vanshajkataria@users.noreply.github.com>
2023-08-28 23:36:16 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import About from '../components/about'
+import Adopt from '../components/adopt'
+import Contact from '../components/contact'
+
+export default function Home() {
+  return (
+    <main>
+      <About/>
+      <Adopt/>
+      <Contact/>
+    </main>
+  );
+}
